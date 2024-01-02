4afcb04a412bfb9c5ca2e4de3ae892120363d805
sukeynacelik1473 <145451091+sukeynacelik1473@users.noreply.github.com>
2024-01-02 11:31:19 +0300

    Add files via upload

---
+++ DATATİME KULLANIMI/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DATATİME_KULLANIMI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "En Küçük: " + DateTime.MinValue;
+            label2.Text = "En Büyük: " + DateTime.MaxValue;
+        }
+    }
+}
